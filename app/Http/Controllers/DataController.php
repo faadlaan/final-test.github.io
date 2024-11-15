1aad6d590703ee4b38bc5bd4b952560bb09bd662
faadlaan <160093276+faadlaan@users.noreply.github.com>
2024-11-15 15:00:20 +0700

    fad

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -35,8 +35,8 @@ class DataController extends Controller
 
         $validated = $request->validate([
             "name" => "required",
-            "email" => "unique:data|required",
-            "phone" => "unique:data|required",
+            "email" => "required|unique:data,email," . $id,
+            "phone" => "required|unique:data,phone," . $id,
             "levels_id" => "required",
         ]);
 
